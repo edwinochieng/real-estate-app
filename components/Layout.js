d6f0aa12053dad0b4d5a6c1f29f4bb6202a3fcc4
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-10-27 02:09:03 +0300

    Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -5,14 +5,14 @@ import Footer from './Footer'
 
 const Layout = ({children}) => {
   return (
-    <div>
+    <div className='px-5 sm:px-8 lg:px-12 xl:px-20 min-h-screen'>
         <Head>
             <title>Real Estate App</title>
         </Head>
         <header>
             <Navbar/>
         </header>
-        <main>
+        <main className='py-4 md:py-10'>
             {children}
         </main>
         <footer>
